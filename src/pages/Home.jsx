397a2410b8accd45f3595abb4a507b6fe8fff4ce
Algi Zaif <algivinciml@gmail.com>
2026-08-16 17:28:41 +0700

    fix case sensitive import achievement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import About from "../components/About/About";
-import Achievement from "../components/Achievement/Achievement";
+import Achievement from "../components/achievement/achievement";
 import Portfolio from "../components/Portfolio/Portfolio";
 
 function Home() {
